aa69527187555a6dbcab89cb93a76402ce65fc6e
Gary Alberto Cordero Rosa <garycordero1690@gmail.com>
2020-01-18 02:40:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,10 +82,12 @@ def adding_matz
         }
      }
 
-     programmer_hash[:yukihiro_matsumoto] = {
-
-      }
-
+     programmer_hash[:adding_matz] = {
+       :yukihiro_matsumoto => {
+           :known_for => "Ruby",
+           :languages => ["LISP", "C"]
+        }
+     }
 end
 
 def changing_alan
